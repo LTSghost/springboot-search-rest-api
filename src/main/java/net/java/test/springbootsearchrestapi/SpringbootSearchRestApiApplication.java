2cf6ef02c2dacb35d24bde536d45f890cf13adac
LTS <DKLTSghost@gmail.com>
2023-07-12 17:34:54 +0800

    can build war

--- src/main/java/net/java/test/springbootsearchrestapi/SpringbootSearchRestApiApplication.java
+++ src/main/java/net/java/test/springbootsearchrestapi/SpringbootSearchRestApiApplication.java
@@ -2,10 +2,15 @@ package net.java.test.springbootsearchrestapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringbootSearchRestApiApplication {
-
+public class SpringbootSearchRestApiApplication extends SpringBootServletInitializer{
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(SpringApplication.class);
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootSearchRestApiApplication.class, args);
 	}
